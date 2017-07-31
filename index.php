45bfd534c46ffb461fd68725dd6067dbe21f9363
Denis <deniskaaa@e-mail.ua>
2017-08-01 00:20:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-echo 'test';
-echo 'second';
-echo 'sdfsf';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$json_data = array('d' => 1);
+$json_data = json_encode($json_data);
+
+$curl_desc = curl_init('http://project.local/json.php');
+
+
+curl_setopt($curl_desc, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl_desc, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($curl_desc, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($curl_desc, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($curl_desc);
+print_R($result);
+
+
+
+ ?>
